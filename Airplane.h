1c61fbe4f07ab8c623823ec39855194d2a6893d7
MVDValeria <lera0050838@gmail.com>
2024-10-13 20:08:14 +0300

    Add files via upload

---
+++ Airplane.h
@@ -0,0 +1,24 @@
+#ifndef AIRPLANE_H
+#define AIRPLANE_H
+
+#include "Transport.h"
+
+class Airplane : public Transport {
+public:
+    Airplane();
+    Airplane(const std::string& type, const std::string& name, double cargoVolume, const std::string& dimensions, const std::string& cities);
+
+    void display() const override;
+    void serialize(std::ostream& os) const override;
+    void deserialize(std::istream& is) override;
+
+private:
+    std::string type;
+    std::string name;
+    double cargoVolume;
+    std::string dimensions;
+    std::string cities;
+};
+
+#endif
+
