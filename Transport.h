5ee0005a9882f20e7b442d808f4772ceea2b2eb1
MVDValeria <lera0050838@gmail.com>
2024-10-17 23:22:01 +0300

    Add files via upload

---
+++ Transport.h
@@ -0,0 +1,17 @@
+#ifndef TRANSPORT_H
+#define TRANSPORT_H
+
+#include <iostream>
+#include <fstream>
+
+class Transport {
+public:
+    virtual ~Transport();
+
+    virtual void display() const = 0;
+    virtual void serialize(std::ostream& os) const = 0;
+    virtual void deserialize(std::istream& is) = 0;
+};
+
+#endif
+
